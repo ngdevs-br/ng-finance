7867c6c637b2d163e391953ddc592bb62a9873cc
Narciso Gomes <narcisoalmeida25@gmail.com>
2025-03-02 23:54:52 -0300

    config(prettier): adiciona prettier no projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { AuthService } from '@services/auth.service';
 
 @Component({
-  selector: 'ng-root',
+  selector: 'app-root', // eslint-disable-line
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  authService = inject(AuthService)
+  authService = inject(AuthService);
   title = 'ng-pessoal-finance';
 }
